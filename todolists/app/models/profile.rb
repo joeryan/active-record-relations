b540408ffa844d6823b555d73a3acb8d5d7e18e0
Joe Ryan <joe@clanryan.us>
2016-04-23 10:30:53 -0400

    add required validators

--- todolists/app/models/profile.rb
+++ todolists/app/models/profile.rb
@@ -1,10 +1,16 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   validate :at_least_one_name
+  validate :no_boys_named_sue
   validates_inclusion_of :gender, in: ["male", "female"]
 
   private
-
+  def no_boys_named_sue
+    if first_name == "Sue" && gender == "male"
+      errors.add(:base, "You can't name a boy Sue!")
+    end
+  end
+  
   def  at_least_one_name
     if first_name.blank? && last_name.blank?
       errors.add(:base, "must have either a first or last name")
