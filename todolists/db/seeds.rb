c7f5ed6784dfc02335080ee5a5bb1d52d7339658
Joe Ryan <joe@clanryan.us>
2016-05-03 20:56:11 -0400

    update project with module 4 starter files (spec, seeds.db, and Gemfile)

--- todolists/db/seeds.rb
+++ todolists/db/seeds.rb
@@ -1,49 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
-# clear database tables
-ActiveRecord::Base.establish_connection
-ActiveRecord::Base.connection.tables.each do |table|
-  ActiveRecord::Base.connection.execute("DELETE FROM #{table}") unless table == "schema_migrations"
+User.destroy_all
+TodoList.destroy_all
+
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
+
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
 end
 
-#populate tables
-user_list = [
-  ["Carly", "Fiorina", "1954", "female"],
-  ["Donald", "Trump", "1946", "male"],
-  ["Ben", "Carson", "1951", "male"],
-  ["Hillary", "Clinton", "1947", "female"]
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
 ]
 
-todos = ["shopping trip", "speech", "argument", "party", "dinner"]
-status = ["boring", "super", "fun", "amazing", "stupid"]
-due_date = Date.today + 1.year
-
-user_list.each do |fname, lname, byear, gender|
-  todolist_name = "#{fname} #{lname}'s' Amazing ToDo List"
-
-  user = User.create(username: lname, password_digest: "#{lname}#{byear}")
-  Profile.create(gender: gender, birth_year: byear, first_name: fname,
-                  last_name: lname, user_id: user.id)
-  tlist = TodoList.create(user_id: user.id,
-                          list_name: todolist_name,
-                          list_due_date: due_date)
-end
-
-users = Profile.all
-users.each do |user|
-  todos.each do |todo|
-    pal = users.sample.first_name
-    TodoItem.create(due_date: due_date,
-                    title: "#{todo} with #{pal}",
-                    description: "Another #{status.sample} #{todo} with #{pal}",
-                    completed: false,
-                    :todo_list_id => TodoList.find_by(user_id: user.user_id).id
-    )
-  end
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
