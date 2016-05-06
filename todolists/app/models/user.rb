b61c11804fc7ea75532c4c026bdebe600aec6348
Joe Ryan <joe@clanryan.us>
2016-05-05 22:30:04 -0400

    add secure_password and controller scaffolding for todo_lists and todo_items

--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :todo_lists, dependent: :destroy
   has_many :todo_items, through: :todo_lists, source: :todo_items
   validates_presence_of :username
+  has_secure_password
 
   def get_completed_count
     TodoItem.where(todo_list_id: TodoList.select(:id).where(user_id: id),
--- todolists/config/routes.rb
+++ todolists/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :todo_lists do
+    resources :todo_items, only: [:create, :destroy]
+  end
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'todo_lists#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
