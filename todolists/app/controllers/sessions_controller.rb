413fcd4d5711b6165976a731c44f8c5a256768f3
Joe Ryan <joe@clanryan.us>
2016-05-07 23:03:09 -0400

    complete sessions controller create action security_spec rq10c

--- todolists/app/controllers/sessions_controller.rb
+++ todolists/app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @username = params[:user][:username]
+    @password = params[:user][:password]
+
+    if (@user = User.find_by(username: @username)) && @user.authenticate(@password)
+      session[:user_id]= @user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Username and/or password incorrect"
+    end
   end
 
   def destroy
